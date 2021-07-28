d409968777110419943bdbd3f1ac1d6b0be95d86
Daniel Feher <feherdaniel2010@gmail.com>
2021-07-28 11:21:45 +0200

    Racket child class implementation, extending Rectangle

---
+++ PingPongGame/PingPongGame/Racket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace PingPongGame
+{
+    class Racket : Rectangle
+    {
+
+        public Racket(System.Windows.Shapes.Rectangle rectangle, MainWindow mainWindow) : base(rectangle, mainWindow)
+        {
+        }
+
+        public void Move(string direction)
+        {
+            var leftMargin = BaseRectangle.Margin.Left;
+            {
+                switch (direction)
+                {
+                    case "Right":
+                        if (leftMargin <= MainWindow.ActualWidth - BaseRectangle.ActualWidth) leftMargin += 10;
+                        break;
+                    case "Left":
+                        if (leftMargin >= -MainWindow.ActualWidth + BaseRectangle.ActualWidth) leftMargin -= 10;
+                        break;
+                }
+            }
+            BaseRectangle.Margin = new Thickness(leftMargin, BaseRectangle.Margin.Top, BaseRectangle.Margin.Right,
+                BaseRectangle.Margin.Bottom);
+        }
+
+
+    }
+}
