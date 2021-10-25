f8255961d29e3bab5bbdf8d209277df9edb870e3
feherdaniel <feherdaniel2010@gmail.com>
2021-10-25 13:50:48 +0200

    refactor: change Rectangle base class name to GameComponent

---
+++ PingPongGame/PingPongGame/GameComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Shapes;
+
+namespace PingPongGame
+{
+    class GameComponent
+    {
+
+        protected MainWindow MainWindow;
+
+        protected Rectangle BaseRectangle;
+
+        public GameComponent(Rectangle rectangle, MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            BaseRectangle = rectangle;
+        }
+
+    }
+}
